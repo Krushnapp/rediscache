a3df74d83173613b3f69893b4efd2678f049c3d6
Krushnapp <krush15699@gamail.com>
2023-09-14 19:06:33 +0530

    Initial Code

---
+++ src/main/java/com/redis/redis/RediscrudApplication.java
@@ -0,0 +1,13 @@
+package com.redis.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RediscrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RediscrudApplication.class, args);
+	}
+
+}
